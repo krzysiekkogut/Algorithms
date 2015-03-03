f744af700e8c326471b762c7bbe0d2dfa0948387
Krzysztof Kogut <krzysiek.kogut@outlook.com>
2015-03-03 16:56:12 +0100

    Russian multiplication tweak - it's enough to remember only parity, not actual results of dividing 'a' by 2.

--- KKogut.Algorithms/KKogut.RussianMultiplication/RussianMultiplication.cs
+++ KKogut.Algorithms/KKogut.RussianMultiplication/RussianMultiplication.cs
@@ -10,10 +10,10 @@ namespace KKogut.RussianMultiplication
     {
         public static long Multiplicate(long a, long b)
         {
-            var A = new List<long>();
+            var A = new List<bool>();
             while (a >= 1)
             {
-                A.Add(a);
+                A.Add(a % 2 == 1);
                 a /= 2;
             }
 
@@ -21,7 +21,7 @@ namespace KKogut.RussianMultiplication
 
             for (int i = 0; i < A.Count; i++)
             {
-                if (A[i] % 2 == 1)
+                if (A[i])
                 {
                     result += b;
                 }
