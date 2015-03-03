d4a316a9a561a4a550830a990ea3b94ac00bb4e7
Krzysztof Kogut <krzysiek.kogut@outlook.com>
2015-03-03 16:54:40 +0100

    Unit test project added.

---
+++ KKogut.Algorithms/KKogut.Algorithms.Tests/RussianMultiplicationTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KKogut.Algorithms.Tests
+{
+    [TestClass]
+    public class RussianMultiplicationTests
+    {
+        [TestMethod]
+        public void Multiplicate_By_0_Returns_0()
+        {
+            long a = 0;
+            long b = 253;
+
+            var result = RussianMultiplication.RussianMultiplication.Multiplicate(a, b);
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Multiplicate_12341234_and_897123_Returns_()
+        {
+            long a = 12341234;
+            long b = 897123;
+
+            var result = RussianMultiplication.RussianMultiplication.Multiplicate(a, b);
+
+            Assert.AreEqual(a * b, result);
+        }
+
+    }
+}
