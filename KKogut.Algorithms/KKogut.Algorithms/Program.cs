b0d55980396192218a8ace565f1d3972ec3bb14d
Krzysztof Kogut <krzysiek.kogut@outlook.com>
2015-03-03 14:14:35 +0100

    Russian multiplication.

---
+++ KKogut.Algorithms/KKogut.Algorithms/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KKogut.Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Please type in a number of algorithm:");
+                var x = Console.ReadLine();
+                switch (x)
+                {
+                    case "0":
+                        RunRussianMultiplication();
+                        break;
+                    default:
+                        return;
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void RunRussianMultiplication()
+        {
+            Console.WriteLine("Enter two natural numbers (delimitted with space):");
+            var @in = Console.ReadLine().Split(' ');
+            Console.WriteLine(RussianMultiplication.RussianMultiplication.Multiplicate(Convert.ToInt64(@in[0]), Convert.ToInt64(@in[1])));
+        }
+    }
+}
---
+++ KKogut.Algorithms/KKogut.RussianMultiplication/RussianMultiplication.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KKogut.RussianMultiplication
+{
+    public static class RussianMultiplication
+    {
+        public static long Multiplicate(long a, long b)
+        {
+            var A = new List<long>();
+            while (a >= 1)
+            {
+                A.Add(a);
+                a /= 2;
+            }
+
+            long result = 0;
+
+            for (int i = 0; i < A.Count; i++)
+            {
+                if (A[i] % 2 == 1)
+                {
+                    result += b;
+                }
+                b *= 2;
+            }
+
+            return result;
+
+        }
+    }
+}
