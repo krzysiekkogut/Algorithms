b7d2134dda1a32e6b15b96474335411d0d695f8f
Krzysztof Kogut <krzysiek.kogut@outlook.com>
2015-03-23 14:36:58 +0100

    Longest common subsequence - memory optimization

--- KKogut.Algorithms/KKogut.LongestCommonSubsequence/LCS.cs
+++ KKogut.Algorithms/KKogut.LongestCommonSubsequence/LCS.cs
@@ -10,20 +10,29 @@ namespace KKogut.LongestCommonSubsequence
     {
         public static int FindLCS(string s1, string s2)
         {
-            var t = new int[s1.Length + 1, s2.Length + 1];
+            if (s1.Length > s2.Length)
+            {
+                var tmp = s1;
+                s1 = s2;
+                s2 = tmp;
+            }
 
-            for (int i = 1; i <= s1.Length; i++)
+            var t = new int[2, s2.Length + 1];
+
+            for (int i = 0; i < s1.Length; i++)
             {
                 for (int j = 1; j <= s2.Length; j++)
                 {
-                    if (s1[i - 1].Equals(s2[j - 1]))
-                        t[i, j] = t[i - 1, j - 1] + 1;
+                    if (s1[i].Equals(s2[j - 1]))
+                        t[1, j] = t[0, j - 1] + 1;
                     else
-                        t[i, j] = Math.Max(t[i - 1, j], t[i, j - 1]);
+                        t[1, j] = Math.Max(t[0, j], t[1, j-1]);
                 }
+                for (int j = 0; j <= s2.Length; j++)
+                    t[0, j] = t[1, j];
             }
 
-            return t[s1.Length, s2.Length];
+            return t[0, s2.Length];
         }
     }
 }
